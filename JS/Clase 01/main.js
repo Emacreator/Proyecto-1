8dc48ef6d60ccd1d1d210d6fb9f1b5b5036f84bb
Emacreator <Ronald_7_9@hotmail.com>
2024-07-31 18:11:23 -0300

    cambie de carpeta

---
+++ JS/Clase 01/main.js
@@ -0,0 +1,128 @@
+//let messi = "Messi";
+//console.log(messi);
+
+//console.log('algo"algo"');
+
+//let nombre = "Messi";
+//console.log(`hola soy ${nombre}`);
+//console.log("hola soy " + nombre);
+
+//prompt("Ingresa nombre");
+
+//let nombreUsuario = "";
+//let nombreUsuario = prompt("Ema");
+//console.log(nombreUsuario);
+
+//var nombreVariable = "Messi";
+//console.log(nombreVariable);
+//var nombreVariable = 10;
+//console.log(nombreVariable);
+
+//const nombreConstante = "Messi campeon del mundo";
+//console.log(nombreConstante);
+
+let numberA = 28;
+let numberB = 36;
+//let result = numberA + numberB;
+
+//console.log(result);
+
+//let textoA = "Hala";
+//let textoB = "Madrid";
+
+//console.log(textoA + textoB);
+
+//console.log(`${textoA} ${textoB}`);
+
+//console.log(`La afición grita ${textoA} ${textoB}`);
+
+//console.log("La afición grita: " + textoA + " " + textoB);
+
+//console.log(numberA + numberB);
+
+//console.log(numberA - numberB);
+
+//console.log(numberA * numberB);
+
+//console.log(numberA / numberB);
+
+//let esPar = numberA & 2;
+//console.log(numberA % 2);
+//console.log(numberB & 3);
+
+//console.log(numberA);
+//numberA++;
+//numberA = numberB + 1;
+//console.log(numberA);
+
+//console.log(numberA);
+//numberA--;
+//console.log(numberA);
+
+//let nombre = prompt("Tu nombre");
+//console.log(nombre);
+
+//Practicas ejercicio NUCBA
+
+//1
+
+//let nombre = "Emanuel";
+//let edad = 29;
+
+//2-3
+
+//nombre = "Messi";
+//edad = "34";
+
+//console.log(nombre, edad);
+
+//console.log("Hola! Mi nombre es " + nombre + " y tengo " + edad + " años");
+
+//console.log(`Hola mi nombre es ${nombre} y tengo ${edad} años`);
+
+//4
+
+//let numberC = 35;
+//let numberD = 45;
+
+//console.log(numberC + numberD);
+
+//5
+
+//const numero = 15;
+//numero = 25;
+//console.log(numero);
+
+//let numero = 15;
+//numero = 25;
+//console.log(numero);
+
+//6
+
+//let number = 15;
+
+//alert(number);
+
+//7
+
+//let valor = prompt("15");
+//console.log(15);
+
+//8
+
+//let valor = prompt("Ingrese un valor");
+//alert("El valor ingresado es " + valor);
+
+//let valor = prompt("ingrese un valor");
+//alert(`el valor ingresado es ${valor}`);
+
+//9
+
+let nombre1 = prompt("ingrese nombre 1");
+let nombre2 = prompt("ingrese nombre 2");
+alert(
+  "El primer nombre ingresado es" +
+    nombre1 +
+    "el segundo nombre ingresado es " +
+    nombre2
+);
--- JS/main.js
+++
@@ -1,2 +0,0 @@
-let messi = "Messi";
-console.log(messi);
