ed7476db8b7334a81e84a26791c7ae7165a56c5e
Emacreator <Ronald_7_9@hotmail.com>
2024-09-03 20:54:08 -0300

    nuevo slider

---
+++ JS/Slider/main.js
@@ -0,0 +1,36 @@
+const slider = document.querySelector(".slider");
+const slides = slider.querySelector(".slides");
+const prevBtn = slider.querySelector(".prev");
+const nextBtn = slider.querySelector(".next");
+
+const slideWith = slides.clientWidth;
+// console.log(slideWith);
+
+let slideIndex = 0;
+
+// Movemos los slides a la posicion indicada por el slideIndex
+const moveToSlide = () => {
+  slides.style.transform = `translateX(${-slideWith * slideIndex}px)`;
+};
+
+//Avanzar al siguiente slide y actualizar la posicion del slider
+const nextSlide = () => {
+  if (slideIndex === slides.children.length - 1) {
+    slideIndex = 0;
+  } else {
+    slideIndex++;
+  }
+  moveToSlide();
+};
+
+const prevSlide = () => {
+  if (slideIndex === 0) {
+    slideIndex = slides.children.length - 1;
+  } else {
+    slideIndex--;
+  }
+  moveToSlide();
+};
+
+nextBtn.addEventListener("click", nextSlide);
+prevBtn.addEventListener("click", prevSlide);
